d8158d0074e086b9b4241071625fe4b9dc7b9ebe
Jakob Steimle <jakob.steimle@tum.de>
2025-11-22 22:00:28 +0100

    Fix temperature

--- build_new_mp_questions.py
+++ build_new_mp_questions.py
@@ -66,7 +66,7 @@ def generate_exam_question_with_openai(
     response = client.beta.chat.completions.parse(
         model="gpt-5",
         messages=messages,
-        temperature=temperature,
+        # temperature=temperature,
         response_format=MultipleChoiceExamQuestion,
     )
 
